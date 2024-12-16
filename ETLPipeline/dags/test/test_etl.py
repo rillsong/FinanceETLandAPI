031eca7ea6835bc10ecc7da0c4ce368627af4688
Jean Zhou <jeanzhou@Jeans-MacBook-Pro.local>
2024-12-16 14:48:43 -0500

    clean test ETL file

--- ETLPipeline/dags/test/test_etl.py
+++ ETLPipeline/dags/test/test_etl.py
@@ -13,9 +13,8 @@ def test_fetch(yfobj:ETL.yfinanceExtractor,tickers,start_date,end_date):
 
 
 def test_transform(df,tickers):
-    df,missing_tickers = ETL.transform_pipeline(df,tickers)
+    df = ETL.transform_pipeline(df,tickers)
     print(df.describe())
-    print(len(missing_tickers))
     return df
 
 def test_loader(dl:ETL.DataLoader,df):   
@@ -27,8 +26,4 @@ def test_loader(dl:ETL.DataLoader,df):
 def test_check_adjclose(yfinanceObj,DataLoaderObj,tickers,start_date,end_date):
     result = ETL.is_adjclose_updated(yfinanceObj,DataLoaderObj,tickers,start_date,end_date)
     print(result)
-
-def test_retry_tickers(yfExtractObj:ETL.yfinanceExtractor,missing_tickers):
-    df = yfExtractObj.retry_failed_tickers(failed_tickers = missing_tickers, start_date = start_date, end_date = end_date, retries = 2)
-    print(df.head())
 
