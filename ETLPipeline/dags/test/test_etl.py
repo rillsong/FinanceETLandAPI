8948c34d55cc9e2ae89fc4374519f72d07374541
Jean Zhou <jeanzhou@Jeans-MacBook-Pro.local>
2024-12-16 09:37:00 -0500

    clean comments; update Dockerfile uvicorn --workers

--- ETLPipeline/dags/test/test_etl.py
+++ ETLPipeline/dags/test/test_etl.py
@@ -32,19 +32,3 @@ def test_retry_tickers(yfExtractObj:ETL.yfinanceExtractor,missing_tickers):
     df = yfExtractObj.retry_failed_tickers(failed_tickers = missing_tickers, start_date = start_date, end_date = end_date, retries = 2)
     print(df.head())
 
-if __name__=="__main__":
-    path = os.path.join(os.getcwd(),"ETLPipeline","dags",r"tickers.csv")
-    sp500_tickers = pd.read_csv(path,usecols=["Symbol"]).squeeze().tolist()
-    start_date = '2024-11-10'
-    end_date = '2024-12-10'
-    
-    DB_URL =  "mysql+pymysql://{user}:{password}@{host}:{port}/{db}".format(**dict(user="root",password = "19990512",host="127.0.0.1",port = "3306", db = "MarketData"))
-    dl = ETL.DataLoader(DB_URL)
-    yfobj = ETL.yfinanceExtractor()
-
-    # test_fetch(yfobj,sp500_tickers,start_date,end_date)
-    df = pd.read_csv("multi_ticker_sample.csv")
-    test_retry_tickers(yfobj,missing_tickers = ["NVDA"])
-    df = test_transform(df,sp500_tickers)
-    test_loader(dl,df)
-    test_check_adjclose(yfobj,dl,sp500_tickers,start_date,end_date)
